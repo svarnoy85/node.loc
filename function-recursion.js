5b3600978c9c8f51af6978f8f7cdb6e3940ad03d
Fedor <svarnoy@bk.ru>
2016-10-22 19:33:48 +0300

    Докоментил рекурсию(пояснения) , почему-то не выкинуть idea из репозитария

--- function-recursion.js
+++ function-recursion.js
@@ -5,12 +5,14 @@ var isEven = function(n) {
     } else if (n%2 == 1) {
         return false;
     } else if (n < 0) {
-        return isEven(-n);
+        return isEven(-n); /* здесь -1 становится 1 (минус на минус даёт плюс) поэтому fales*/
     } else {
-        return isEven(n - 2);
+        return isEven(n - 2); /* это работает, т.к. мы отнимаем от числа 2, если от четного забирать, то оно и останется четным или нулём*/
     }
 }
 
+console.log(isEven(0));
+// → true
 console.log(isEven(50));
 // → true
 console.log(isEven(75));
