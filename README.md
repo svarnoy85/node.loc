d60afa8eb11a280991a29adc8c3e8b4d0f59c934
Fedor <svarnoy@bk.ru>
2016-10-22 22:14:02 +0300

    Подправил README. Занёс  countBs и countChar

---
+++ README.md
@@ -0,0 +1,22 @@
+# node.loc
+**Задачки по js**<br/>
+Источник http://www.itmathrepetitor.ru/zadachi-po-javascript-iz-knigi-vyrazitelnyjj-javascript/<br/>
+Сделано следующее:<br/><br/>
+1. Треугольник - treygolnik.js<br/>
+2. Шахматная доска - shaxmaty.js<br/>
+3. FizzBuzz - fizzbuss.js<br/>
+4. Минимум - function-min.js<br/>
+5. Рекурсия - function-recursion.js<br/>
+6. Считаем бобы - function-letters.js<br/>
+7. Сумма диапазона - function-summa.js
+
+
+
+   
+
+   
+
+
+  
+
+
--- function-letters.js
+++ function-letters.js
@@ -1,15 +1,14 @@
 "use strict";
 function Kolvo(string, n) {
 
-    var x = string.length;
-    console.log('Всего букв в слове: ' + x);    /* это выведет кол-во букв в слове*/
+    var countBs = string.length;
+    console.log('Всего букв в слове: ' + countBs);    /* это выведет кол-во букв в слове*/
 
-    var count = 0;
+    var countChar = 0;
     for (var i = 0; i < string.length; i++) /*здесь мы получаем длину слова и соответственно кол-во проходов, чтобы сосчитать кол-во букв в слове*/
         if (string.charAt(i) == n) /* если нашлась буква*/
-            count += 1; /* добавляем +1 в счётчик кол-ва(count)*/
-    return count;
-
+            countChar += 1; /* добавляем +1 в счётчик кол-ва(count)*/
+    return countChar;
 }
 
 console.log(Kolvo("kakkerlak", "k"));  /*тут слово и что ищем*/
