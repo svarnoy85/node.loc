80089237589c5e23c31486aa1b10cee97d735a9a
svarnoy85 <svarnoy@bk.ru>
2016-10-22 15:36:39 +0300

    3 commit

--- shaxmaty.js
+++ shaxmaty.js
@@ -1,40 +1,14 @@
 "use strict";
-//треугольник
-function treug() {
-    var str= "#"
-    for(var i=0; i<7; i++){
-        console.log(str)
-        str=str+"#"
-    }
-}
-treug()
-//доска халявная :)
-function doska() {
-    var y="# # # # # #"
-    var x=" # # # # # "
-    for (var i=0; i<6; i++){
-        console.log(y)
-        console.log(x)
-    }
-}
-doska()
-
 function doskamax() {
-    var x="#"
-    var y=" "
+    var x=" "
+    var y="#"
     for (var i=0; i<8; i++){
         if( i%2 ){
-
-                var para=x+y;
-            var stroka= para.repeat(4);
-
+            var ryad= (x+y).repeat(4);
         } else {
-
-                var para=y+x;
-            var stroka= para.repeat(4);
-
+            var ryad= (y+x).repeat(4);
         }
-        console.log(stroka)
+        console.log(ryad)
 
     }
 
--- treygolnik.js
+++ treygolnik.js
@@ -1,5 +1,4 @@
 "use strict";
-//треугольник
 function treug() {
     var str= "#"
     for(var i=0; i<7; i++){
@@ -8,35 +7,3 @@ function treug() {
     }
 }
 treug()
-//доска халявная :)
-function doska() {
-    var y="# # # # # #"
-    var x=" # # # # # "
-    for (var i=0; i<6; i++){
-        console.log(y)
-        console.log(x)
-    }
-}
-doska()
-
-function doskamax() {
-    var x="#"
-    var y=" "
-    for (var i=0; i<8; i++){
-        if( i%2 ){
-
-                var para=x+y;
-            var stroka= para.repeat(4);
-
-        } else {
-
-                var para=y+x;
-            var stroka= para.repeat(4);
-
-        }
-        console.log(stroka)
-
-    }
-
-}
-doskamax()
